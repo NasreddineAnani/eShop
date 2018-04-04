a485a9021c4d895e75c1222218d67aabf5130106
Hugues Soares <soarezzz77@gmail.com>
2018-04-04 14:13:48 -0400

    modif script et sqlData.txt

--- ScriptMysql.py
+++ ScriptMysql.py
@@ -7,22 +7,19 @@ file.close()
 db1 = pymysql.connect(host="localhost",user="root",password="mysql")
 cursor = db1.cursor()
 
-# query1 = 'CREATE DATABASE test18;'
-# query2 = 'USE test18;'
-# query3 = 'CREATE TABLE `test18` (`idProduct` INT UNSIGNED NOT NULL, `price` FLOAT NOT NULL, `description` MEDIUMTEXT NOT NULL, `name` VARCHAR(45) NOT NULL, `type` VARCHAR(45) NOT NULL, `imgUrl` MEDIUMTEXT NOT NULL,   PRIMARY KEY (`idProduct`));'
-#
-# cursor.execute(query1)
-# cursor.execute(query2)
-# cursor.execute(query3)
+query1 = 'CREATE DATABASE eShop;'
+query2 = 'USE eShop;'
+query3 = 'CREATE TABLE `products` (`idProduct` INT UNSIGNED NOT NULL, `price` FLOAT NOT NULL, `description` MEDIUMTEXT NOT NULL, `name` VARCHAR(45) NOT NULL, `type` VARCHAR(45) NOT NULL, `imgUrl` MEDIUMTEXT NOT NULL,   PRIMARY KEY (`idProduct`));'
+
+cursor.execute(query1)
+cursor.execute(query2)
+cursor.execute(query3)
+
 cursor.close()
 db1.close()
 
-db2 = pymysql.connect(host="localhost", user="root", password="mysql", db="test18")
+db2 = pymysql.connect(host="localhost", user="root", password="mysql", db="eShop")
 cursor2 = db2.cursor()
-# query = "INSERT INTO test18 (idProduct, price, description, name, type, imgUrl) VALUES (1, 1759.89, 'Sometimes water boy want within.', 'Scott Marshall and Sullivan', 'hawaii', 'https://images.unsplash.com/photo-1466501802958-fa215d854df3?ixlib=rb-0.3.5&ixidProduct=eyJhcHBfaWQiOjEyMDd9&s=fb13603c544d540da9117097b2473d20&auto=format&fit=crop&w=440');"
-# print(query)
-# cursor2.execute(query)
-# db2.commit()
 
 for line in lines:
     print(line)
